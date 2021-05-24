6fc721224e0a1ee05ba024c2a393480aad3a00fc
DrgOv <ovidiu.dragoi00@gmail.com>
2021-05-24 10:32:52 +0300

    added react-fullpage.js

--- src/components/screens/Main.jsx
+++ src/components/screens/Main.jsx
@@ -4,18 +4,44 @@ import { Home } from './home/Home.jsx';
 import { Portfolio } from './portfolio/Portfolio.jsx';
 import bgMusic from '../../res/sound/bg-cyberspace.mp3';
 import MediaUtils from './../../utils/MediaUtils';
+import ReactFullpage from '@fullpage/react-fullpage';
 
 export const Main = () => {
 	useEffect(() => {
 		const audio = document.querySelector('.Main__audio');
 		MediaUtils.audioPlayFadeIn(audio);
 	});
-	return (
-		<React.StrictMode>
-			<audio className='Main__audio' src={bgMusic} loop />
-			<Home />
-			<Portfolio />
-			<Contact />
-		</React.StrictMode>
-	);
+	const smallScreenRender = () => {
+		return (
+			<React.StrictMode>
+				<Home />
+				<Portfolio />
+				<Contact />
+				<audio className='Main__audio' src={bgMusic} loop />
+			</React.StrictMode>
+		);
+	};
+	const bigScreenRender = () => {
+		return (
+			<ReactFullpage
+				render={() => {
+					return (
+						<ReactFullpage.Wrapper>
+							<div className='section'>
+								<Home />
+							</div>
+							<div className='section'>
+								<Portfolio />
+							</div>
+							<div className='section'>
+								<Contact />
+							</div>
+							<audio className='Main__audio' src={bgMusic} loop />
+						</ReactFullpage.Wrapper>
+					);
+				}}
+			/>
+		);
+	};
+	return window.screen.width >= 1024 ? bigScreenRender() : smallScreenRender();
 };
